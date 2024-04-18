7df1e6c5502f63e702769f6b1352de05ae4b5dd5
mihajloalimpic <mihajlo.alimpic@student.um.si>
2024-04-18 14:12:25 +0200

    React Native Maps

--- explorerai/App.js
+++ explorerai/App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+import { markers } from './markers';
+import { Alert } from 'react-native';
+
+const INITIAL_REGION = {
+  latitude: 46.5547,
+  longitude: 15.6459,
+  latitudeDelta: 0.05,
+  longitudeDelta: 0.05,
+} 
+
+const onMarkerClick = (marker) => {
+  Alert.alert(marker.name);
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+        style={styles.map} 
+        provider={PROVIDER_GOOGLE} 
+        initialRegion={INITIAL_REGION}
+        showsUserLocation
+        showsMyLocationButton
+      >
+        {markers.map((marker, index) => (
+          <Marker key={index} coordinate={marker} onPress={() => onMarkerClick(marker)}/>
+        )
+      )}
+      </MapView>
     </View>
   );
 }
@@ -13,8 +39,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
---
+++ explorerai/markers.ts
@@ -0,0 +1,37 @@
+export const markers = [
+    {
+        latitude: 46.5591,
+        longitude: 15.6446,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+        name: "Glavni trg"
+    },
+    {
+        latitude: 46.5571,
+        longitude: 15.6455,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+        name: "Mariborski grad"
+    },
+    {
+        latitude: 46.5585,
+        longitude: 15.6468,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+        name: "Cerkev svetega Janeza Krstnika"
+    },
+    {
+        latitude: 46.5567,
+        longitude: 15.6479,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+        name: "Univerza v Mariboru"
+    },
+    {
+        latitude: 46.5610,
+        longitude: 15.6500,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+        name: "Mestni park"
+    }
+]
