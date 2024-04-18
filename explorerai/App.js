beee4f765912e728f77e69d394762d382c163032
Katarina Lipovsek <katarina.lipovsek2@student.um.si>
2024-04-18 21:12:47 +0200

    posodobljena struktura

--- explorerai/App.js
+++ explorerai/App.js
@@ -1,37 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
+
 import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { Marker } from 'react-native-maps';
-import { markers } from './markers';
-import { Alert } from 'react-native';
-
-const INITIAL_REGION = {
-  latitude: 46.5547,
-  longitude: 15.6459,
-  latitudeDelta: 0.05,
-  longitudeDelta: 0.05,
-} 
-
-const onMarkerClick = (marker) => {
-  Alert.alert(marker.name);
-};
+import { StyleSheet, View } from 'react-native';
+import MapScreen from './Components/MapScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView 
-        style={styles.map} 
-        provider={PROVIDER_GOOGLE} 
-        initialRegion={INITIAL_REGION}
-        showsUserLocation
-        showsMyLocationButton
-      >
-        {markers.map((marker, index) => (
-          <Marker key={index} coordinate={marker} onPress={() => onMarkerClick(marker)}/>
-        )
-      )}
-      </MapView>
+      <MapScreen />
     </View>
   );
 }
@@ -40,8 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
 });
+
+registerRootComponent(App);
+
