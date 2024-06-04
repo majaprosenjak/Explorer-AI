cb8b2d13c0f1d9b5b4002ae59f8a901e87763e96
mihajloalimpic <mihajlo.alimpic@student.um.si>
2024-06-04 23:38:27 +0200

    Directions dodano

--- explorerai/Components/MapPage.js
+++ explorerai/Components/MapPage.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { StyleSheet, View, Alert, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Alert } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import { markers } from './markers';
 
 const INITIAL_REGION = {
@@ -10,12 +11,26 @@ const INITIAL_REGION = {
   longitudeDelta: 0.05,
 };
 
-const onMarkerClick = (marker) => {
-  Alert.alert(marker.name, marker.description);
-};
-
+const GOOGLE_MAPS_APIKEY = 'AIzaSyDcJdWLYO_2ueX-G6D9Z-CoF_dFfbsb7rA';
 
 const MapPage = () => {
+  const [directions, setDirections] = useState([]);
+
+  useEffect(() => {
+    const createDirections = () => {
+      if (markers.length > 1) {
+        const startMarker = markers[0];
+        const newDirections = markers.slice(1).map(marker => ({
+          origin: { latitude: startMarker.latitude, longitude: startMarker.longitude },
+          destination: { latitude: marker.latitude, longitude: marker.longitude },
+        }));
+        setDirections(newDirections);
+      }
+    };
+
+    createDirections();
+  }, []);
+
   return (
     <View style={styles.container}>
       <MapView
@@ -26,7 +41,21 @@ const MapPage = () => {
         showsMyLocationButton
       >
         {markers.map((marker, index) => (
-          <Marker key={index} coordinate={marker} onPress={() => onMarkerClick(marker)} />
+          <Marker
+            key={index}
+            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+            onPress={() => Alert.alert(marker.name, marker.description)}
+          />
+        ))}
+        {directions.map((direction, index) => (
+          <MapViewDirections
+            key={index}
+            origin={direction.origin}
+            destination={direction.destination}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={3}
+            strokeColor="hotpink"
+          />
         ))}
       </MapView>
     </View>
